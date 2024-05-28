9b6496bbaac06a84678a387fbefb694b58f4a926
Brirosman <brirosman10@gmail.com>
2024-05-28 09:06:44 -0300

    Password

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public string Contraseña = "prit";
+    public InputField IngresarContraseña;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Contra()
+    {
+        if (IngresarContraseña = Contraseña)
+        {
+            Debug.Log("Access garanted");
+        }
+        else
+        {
+            Debug.Log("Access denied");
+        }
+}
+}
